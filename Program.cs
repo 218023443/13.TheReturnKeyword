b752e0fbffcf04e0bfe53d5c3c60a085babf4253
A252557 <A252557@LSBSA-3WCCHW3>
2023-12-06 14:48:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.TheReturnKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(cube(5));
+            Console.ReadLine();
+
+        }
+        //declare a variable with a return type int
+        static int cube(int number)
+        {
+            int result = number * number * number;
+            return result;
+
+        }
+
+    }
+    }
+
